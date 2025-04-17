d6b1cb784afb325b483e8d038d0e2206968446e1
LeeJunYoung1222 <ljy@itsolutionkr.com>
2025-04-17 10:17:34 +0900

    feat: tsconfig설정 및 queryClient설정 1. tsconfig에 baseUrl을 설정 2. 기본으로 사용할 queryClient의 defaultOptions 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
